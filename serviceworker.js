2d3278fdde4c62d3905e66fc16b16543927e8036
varun_mishra1 <varun_mishra1@epam.com>
2025-03-17 05:36:38 +0530

    done

--- serviceworker.js
+++ serviceworker.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = "pwa-cache-v1";
 const urlsToCache = [
   "/",
-  "/index.html",
-  "/script.js",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
+  "index.html",
+  "script.js",
+  "icons/icon-192x192.png",
+  "icons/icon-512x512.png"
 ];
 
 self.addEventListener("install", (event) => {
